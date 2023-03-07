00cca62ed505bb7041d18b2d6ada9e736c105d82
prabal-github <prabal.chow09009.pc@gmail.com>
2023-03-07 23:00:42 +0530

    add read more

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 'use client';
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
+import { Pagination } from '@/components/Pagination';
 import Image from 'next/image'
 import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 import readingTime from 'reading-time'
 import Moment from 'react-moment'
-
+import Link from 'next/link';
 export default function Home() {
   const { data, error, isLoading } = useSWR(
     "https://the-hit-times-admin-production.up.railway.app/api/posts?limit=10",
@@ -44,6 +45,8 @@ export default function Home() {
                   <div className='text-xs'>
                     {readingTime(post.body).text}
                   </div>
+                  <div className='mx-1.5'>&#x2022;</div>
+                  <Link href={'/'} className='hover:underline hover:text-blue-800'>Read more</Link>
                 </div>
               </div>
             </div>
@@ -51,31 +54,7 @@ export default function Home() {
         </div>
       </div>
 
-      <nav aria-label="Page navigation example" className='mb-10 flex items-center justify-center'>
-        <ul className="inline-flex -space-x-px">
-          <li>
-            <a href="#" className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
-          </li>
-          <li>
-            <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-          </li>
-          <li>
-            <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-          </li>
-          <li>
-            <a href="#" aria-current="page" className="px-3 py-2 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-          </li>
-          <li>
-            <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
-          </li>
-          <li>
-            <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-          </li>
-          <li>
-            <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
-          </li>
-        </ul>
-      </nav>
+      <Pagination />
 
       <Footer />
     </main>
