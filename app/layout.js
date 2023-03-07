131fd8f255e5d11e3adec0c7d3cb0b5cb1852877
prabal-github <prabal.chow09009.pc@gmail.com>
2023-03-07 22:53:50 +0530

    should work

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,4 @@
-'use client'
 import './globals.css'
-import Head from 'next/head'
 export const metadata = {
   title: 'THT App Design',
   description: 'This is the THT App Design',
@@ -9,9 +7,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="shortcut icon" href="../public/logo.jpg" />
-      </Head>
       <body className='mx-auto max-w-screen-sm h-screen bg-gray-100 text-tertiary-400'>{children}</body>
     </html>
   )
